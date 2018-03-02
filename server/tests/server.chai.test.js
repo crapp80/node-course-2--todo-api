a55c4eba9e1e45db49824344a1c39aaa8a91fc90
crapp80 <carsten.rapp@icloud.com>
2018-03-02 21:05:32 +0100

    added Test POST /todos using Chai

--- server/tests/server.chai.test.js
+++ server/tests/server.chai.test.js
@@ -1,53 +1,56 @@
 /* disable eslint errors for test syntax */
 /* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
-const expect = require('expect');
-const request = require('supertest');
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
 
 const { app } = require('../server');
-const { Todo } = require('../models/todo');
+const { Todo } = require('../models/todo.js');
+
+const should = chai.should();
+
+chai.use(chaiHTTP);
 
 // make sure the database is empty before every request
 beforeEach((done) => {
   Todo.remove().then(() => done());
 });
 
-describe('POST /todos', () => {
+describe('* USING CHAI * POST /todos', () => {
   it('should create a new todo', (done) => {
     const text = 'Test todo test';
 
-    request(app)
+    chai.request(app)
       .post('/todos')
       .send({ text })
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.text).toBe(text);
-      })
-      .end((err) => {
+      .end((err, res) => {
         if (err) {
           return done(err);
         }
 
+        res.should.have.status(200);
+        res.body.text.should.equal(text);
         return Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
+          todos.should.have.lengthOf(1);
+          todos[0].text.should.equal(text);
           done();
         }).catch(e => done(e));
       });
   });
 
-  it('should not create todo with invaild body data', (done) => {
-    request(app)
+  it('should not create todo with invalid body data', () => {
+    chai.request(app)
       .post('/todos')
       .send({})
-      .expect(400)
-      .end((err) => {
+      .end((err, res) => {
         if (err) {
           return done(err);
         }
 
+        res.should.have.status(400);
         return Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          todos.should.have.lengthOf(0);
           done();
         }).catch(e => done(e));
       });
