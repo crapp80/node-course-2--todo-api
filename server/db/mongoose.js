8070ed3001dfbb0f90cacf4689084b98805948bd
crapp80 <carsten.rapp@icloud.com>
2018-03-04 22:37:27 +0100

    preparements for heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // so we can use Promises instead of callbacks
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// in CL: "heroku addons:create mongolab:sandbox", heroku config to see MONGODB_URI
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
