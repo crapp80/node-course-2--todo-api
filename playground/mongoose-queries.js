a7b32cca1da726fd315a17ee54f19e73b0e21e6c
crapp80 <carsten.rapp@icloud.com>
2018-03-04 18:31:56 +0100

    Added queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('../server/db/mongoose');
+const { Todo } = require('../server/models/todo');
+const { User } = require('../server/models/user');
+
+const id = '5a987347236c9c5fa2798828';
+
+if (!ObjectID.isValid(id)) {
+  console.log('ID not valid.');
+}
+
+// Todo.find({
+//   _id: id,
+// }).then((todos) => {
+//   console.log('Todos: ', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id,
+// }).then((todo) => {
+//   console.log('Todo: ', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('ID not found.');
+//   }
+//   return console.log('Todo by ID: ', todo);
+// }).catch(e => console.log(e));
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('Unable to find user.');
+  }
+  return console.log('User by ID: ', user);
+}).catch(e => console.log(e));
