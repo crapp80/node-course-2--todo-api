777991b4832b91e7dce68107023a0617070d1f45
crapp80 <carsten.rapp@icloud.com>
2018-03-05 08:59:37 +0100

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -16,6 +17,7 @@ app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text,
   });
+
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
@@ -33,9 +35,11 @@ app.get('/todos', (req, res) => {
 
 app.get('/todos/:id', (req, res) => {
   const { id } = req.params;
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send('ID is not valid.');
   }
+
   return Todo.findById(id)
     .then((todo) => {
       if (!todo) {
@@ -49,9 +53,11 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res) => {
   const { id } = req.params;
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send('ID is not valid.');
   }
+
   return Todo.findByIdAndRemove(id)
     .then((todo) => {
       if (!todo) {
@@ -61,6 +67,32 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
       res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('ID is not valid.');
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  return Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    return res.send({ todo });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
