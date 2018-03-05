73704bff651a581a96ebe63c3d61d17ca8ae4cb1
crapp80 <carsten.rapp@icloud.com>
2018-03-05 16:12:00 +0100

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // so we can use Promises instead of callbacks
 // in CL: "heroku addons:create mongolab:sandbox", heroku config to see MONGODB_URI
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
